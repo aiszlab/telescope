d7c671e3f85fd96b360c01f422682838094ac847
murukal <tutu@fantufantu.com>
2023-12-10 22:09:56 +0800

    feat: update some bugs

--- demos/tree/basic/index.tsx
+++ demos/tree/basic/index.tsx
@@ -1,9 +1,33 @@
 'use client'
 
-import { Tree } from 'musae'
+import { Tree, TreeProps } from 'musae'
+
+const nodes: TreeProps['nodes'] = [
+  {
+    key: 'zhejiang',
+    title: '浙江',
+    children: [
+      {
+        key: 'hangzhou',
+        title: '杭州',
+        children: [
+          {
+            key: 'xihu',
+            title: '西湖区'
+          }
+        ]
+      },
+      {
+        key: 'shaoxing',
+        title: '绍兴',
+        children: []
+      }
+    ]
+  }
+]
 
 const Basic = () => {
-  return <Tree />
+  return <Tree nodes={nodes} />
 }
 
 export default Basic
