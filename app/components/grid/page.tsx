ab160ee3250bcbf2470bb7954166a2a2f94a6011
murukal <tutu@fantufantu.com>
2023-12-17 19:57:35 +0800

    feat: use demo

--- app/components/grid/page.tsx
+++ app/components/grid/page.tsx
@@ -1,12 +1,15 @@
+'use client'
+
 import ComponentDocument from '@/components/component-document'
 import BasicDemo from '@/demos/input/basic'
-import BasicCode from '@/demos/input/basic/demo.mdx'
+import BasicCode from '@/demos/input/basic?raw'
 import Properties from '@/demos/input/properties.mdx'
 
 const Page = () => {
   return (
     <ComponentDocument
-      title='输入框'
+      title='Grid 栅格'
+      description='在多数业务情况下，需要在设计区域内解决大量信息收纳的问题，musae 将整个设计建议区域按照 24 等分的原则进行划分。'
       codeDemos={[
         {
           demo: <BasicDemo />,
