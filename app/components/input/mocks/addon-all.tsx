c3366f8c943fbfb71ea78bf896ec7a724ee7255f
murukal <tutu@fantufantu.com>
2024-01-03 23:59:09 +0800

    feat: add input demo

---
+++ app/components/input/mocks/addon-all.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { Input } from 'musae'
+
+const AddonAll = () => {
+  return <Input type='text' prefix='￥' suffix='RMB' />
+}
+
+export default AddonAll
---
+++ app/components/input/mocks/basic.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { Input } from 'musae'
+
+const Basic = () => {
+  return <Input type='text' />
+}
+
+export default Basic
