a317cd42e74b1dc98cad2b4df653cf388fb4164d
murukal <tutu@fantufantu.com>
2024-01-21 20:12:30 +0800

    feat: remove useless code

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -4,7 +4,6 @@ import { useCallback, type Key, useMemo } from 'react'
 import { Menu, Layout as _Layout } from 'musae'
 import { useComponentTree } from '@/hooks/components.hook'
 import { useRouter } from 'next/navigation'
-import CacheProvider from '@/components/cache-provider'
 
 interface Props {
   children: string
@@ -26,15 +25,13 @@ const Layout = (props: Props) => {
   }, [menuItems])
 
   return (
-    <CacheProvider>
-      <_Layout>
-        <_Layout.Sider className='px-3 sticky top-0 overflow-hidden hover:overflow-y-auto'>
-          <Menu items={menuItems} onClick={onMenuClick} defaultExpandedKeys={defaultExpandedKeys} />
-        </_Layout.Sider>
+    <_Layout>
+      <_Layout.Sider className='px-3 sticky top-0 overflow-hidden hover:overflow-y-auto'>
+        <Menu items={menuItems} onClick={onMenuClick} defaultExpandedKeys={defaultExpandedKeys} />
+      </_Layout.Sider>
 
-        <_Layout.Main className='p-8 pt-0'>{props.children}</_Layout.Main>
-      </_Layout>
-    </CacheProvider>
+      <_Layout.Main className='p-8 pt-0'>{props.children}</_Layout.Main>
+    </_Layout>
   )
 }
 
