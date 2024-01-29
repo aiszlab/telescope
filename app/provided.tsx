78b48354c2992d2389322728d9f437e085669379
murukal <tutu@fantufantu.com>
2024-01-29 23:21:04 +0800

    chore: remove useless import

--- app/provided.tsx
+++ app/provided.tsx
@@ -2,7 +2,7 @@
 
 import { Layout as _Layout, ThemeProvider } from 'musae'
 import Link from 'next/link'
-import { useEffect, type ReactNode, useRef } from 'react'
+import { type ReactNode } from 'react'
 
 interface Props {
   children?: ReactNode
