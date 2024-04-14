0e4933e021dc3595b47f1f5f5d07ef41e75f5ae4
murukal <tutu@fantufantu.com>
2024-04-14 22:21:47 +0800

    feat: complete form docs

--- mocks/components/form/instance.tsx
+++ mocks/components/form/instance.tsx
@@ -21,34 +21,37 @@ const Instance = () => {
     if (!isValid) return
 
     const values = form.getValues()
-    info(`用户名: ${values}`)
+    info(`用户名: ${values.username}; 密码: ${values.password}`)
   }
 
   const fill = () => {
-    form.setValue('username', 'murukal')
-    form.setValue('password', 'murukal-password')
+    form.setValue('username', 'murukal', { shouldValidate: true })
+    form.setValue('password', 'murukal-password', { shouldValidate: true })
   }
 
   return (
-    <Form form={form}>
-      <Item label='用户名' required name='username'>
-        <Input />
-      </Item>
+    <>
+      <Form form={form}>
+        <Item label='用户名' required name='username'>
+          <Input />
+        </Item>
 
-      <Item label='密码' required name='password'>
-        <Input type='password' />
-      </Item>
+        <Item label='密码' required name='password'>
+          <Input type='password' />
+        </Item>
 
-      <Item>
-        <Space>
-          <Button onClick={login}>登录</Button>
-          <Button onClick={reset}>重置</Button>
-          <Button onClick={fill} variant='text'>
-            填充表单
-          </Button>
-        </Space>
-      </Item>
-    </Form>
+        <Item>
+          <Space>
+            <Button onClick={login}>登录</Button>
+            <Button onClick={reset}>重置</Button>
+            <Button onClick={fill} variant='text'>
+              填充表单
+            </Button>
+          </Space>
+        </Item>
+      </Form>
+      {holder}
+    </>
   )
 }
 
