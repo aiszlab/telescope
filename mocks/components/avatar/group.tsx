431325a215d88776ba3768be07d5a9add9dea1d0
murukal <tutu@fantufantu.com>
2024-06-12 08:32:06 +0800

    chore: improve docs api

--- mocks/components/avatar/group.tsx
+++ mocks/components/avatar/group.tsx
@@ -1,15 +1,23 @@
 'use client'
 
-import { Avatar } from 'musae'
+import { Avatar, Space } from 'musae'
 
 const App = () => {
   return (
-    <Avatar.Group>
-      <Avatar src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
-      <Avatar src='https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80' />
-      <Avatar src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
-      <Avatar src='https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80' />
-    </Avatar.Group>
+    <Space type='vertical'>
+      <Avatar.Group>
+        <Avatar src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
+        <Avatar src='https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80' />
+        <Avatar src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
+      </Avatar.Group>
+      <Avatar.Group>
+        <Avatar src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
+        <Avatar src='https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80' />
+        <Avatar src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
+        <Avatar src='https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80' />
+        <Avatar src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
+      </Avatar.Group>
+    </Space>
   )
 }
 
--- mocks/components/popover/trigger-by.tsx
+++ mocks/components/popover/trigger-by.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { Popover, Space } from 'musae'
 
 const App = () => {
