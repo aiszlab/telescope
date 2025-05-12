8c90bba4e561c730d605ff3f271ad7f9f45a7796
murukal <tutu@fantufantu.com>
2025-05-12 11:02:06 +0800

    refactor: standardize formatting and improve code consistency in basic form component

--- mocks/components/form/basic.tsx
+++ mocks/components/form/basic.tsx
@@ -1,24 +1,26 @@
-'use client'
-import { Form, Input, Button } from 'musae'
+"use client";
+import { Form, Input, Button } from "musae";
 
-const Item = Form.Item
+const Item = Form.Item;
 
 const App = () => {
+  const form = Form.useForm();
+
   return (
-    <Form>
-      <Item label='用户名' required name='username'>
+    <Form form={form}>
+      <Item label="用户名" required name="username">
         <Input />
       </Item>
 
-      <Item label='密码' required name='password'>
-        <Input type='password' />
+      <Item label="密码" required name="password">
+        <Input type="password" />
       </Item>
 
       <Item>
-        <Button type='submit'>登录</Button>
+        <Button type="submit">登录</Button>
       </Item>
     </Form>
-  )
-}
+  );
+};
 
-export default App
+export default App;
