bbe669597d6ba93ff164d8f855edcddea4cb3214
murukal <tutu@fantufantu.com>
2025-05-12 11:06:51 +0800

    refactor: improve code formatting and consistency in instance component

--- mocks/components/form/instance.tsx
+++ mocks/components/form/instance.tsx
@@ -1,50 +1,52 @@
-'use client'
-import { Form, Input, Space, Button, useMessage } from 'musae'
+"use client";
+import { Form, Input, Space, Button, useMessage } from "musae";
 
-const { Item, useForm } = Form
+const { Item, useForm } = Form;
 
 interface FormValues {
-  username: string
-  password: string
+  username: string;
+  password: string;
 }
 
 const Instance = () => {
-  const form = useForm<FormValues>()
-  const [{ info }, holder] = useMessage()
+  const form = useForm<FormValues>();
+  const [{ info }, holder] = useMessage();
 
   const reset = () => {
-    form.reset()
-  }
+    form.reset();
+  };
 
   const login = async () => {
-    const isValid = await form.trigger().catch(() => false)
-    if (!isValid) return
+    const isValid = await form.validate().catch(() => false);
+    if (!isValid) return;
 
-    const values = form.getValues()
-    info({ description: `用户名: ${values.username}; 密码: ${values.password}` })
-  }
+    const values = form.getFieldsValue();
+    info({
+      description: `用户名: ${values.username}; 密码: ${values.password}`,
+    });
+  };
 
   const fill = () => {
-    form.setValue('username', 'murukal', { shouldValidate: true })
-    form.setValue('password', 'murukal-password', { shouldValidate: true })
-  }
+    form.setFieldValue("username", "murukal");
+    form.setFieldValue("password", "murukal-password");
+  };
 
   return (
     <>
       <Form form={form}>
-        <Item label='用户名' required name='username'>
+        <Item label="用户名" required name="username">
           <Input />
         </Item>
 
-        <Item label='密码' required name='password'>
-          <Input type='password' />
+        <Item label="密码" required name="password">
+          <Input type="password" />
         </Item>
 
         <Item>
           <Space>
             <Button onClick={login}>登录</Button>
             <Button onClick={reset}>重置</Button>
-            <Button onClick={fill} variant='text'>
+            <Button onClick={fill} variant="text">
               填充表单
             </Button>
           </Space>
@@ -52,7 +54,7 @@ const Instance = () => {
       </Form>
       {holder}
     </>
-  )
-}
+  );
+};
 
-export default Instance
+export default Instance;
